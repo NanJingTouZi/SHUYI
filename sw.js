646a4f7e5fd8114aeff32685b7efc3a2e42fff9b
NanJing <isanwijaya82@gmail.com>
2026-07-14 01:55:10 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+// sw.js - Service Worker untuk PWA
+const CACHE_NAME = 'life-number-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache or network
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate and clean old caches
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
